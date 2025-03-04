f2b53330159d31583a28ce0ab26ae75b68c883b8
Raphael Correia Dornelas <dornelasrapha@gmail.com>
2025-03-03 23:39:56 -0300

    add doc aresta

--- codigo/tAresta.h
+++ codigo/tAresta.h
@@ -11,12 +11,25 @@ typedef struct aresta tAresta;
 /// @return a aresta criada
 tAresta *criaAresta(tVertice* destino, float peso);
 
+/// @brief libera a memoria utilizada por a
+/// @param a uma aresta valida
 void apagaAresta(void* a);
 
+/// @brief exibe o conteudo de a no terminal
+/// @param a uma aresta 
+/// @param b um dado qualquer
+/// @return 1
 int ImprimeAresta(void* a, void* b);
 
+/// @brief obtem o peso de a
+/// @param a uma aresta valida
+/// @return o valor do custo para
+/// chegar ao destino de a
 float getPesoAresta(tAresta *a);
 
+/// @brief obtem o destino de a 
+/// @param a uma aresta valida
+/// @return o vertice onde a incide
 tVertice* getDestinoAresta(tAresta *a);
 
 #endif
